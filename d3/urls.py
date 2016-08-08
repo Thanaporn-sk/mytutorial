e5e0e0b3297b7ccfc98083ebe4973534730ae4e4
Thanaporn-sk <thanaporn.sk@gmail.com>
2016-08-08 16:59:28 +0700

    add barchart ,sunburst
    add barchart ,sunburst

--- d3/urls.py
+++ d3/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     #url(r'^$', views.post_list, name='post_list'),
     url(r'^d3$', views.d3Chain, name='d3Chain'),
     url(r'^d3circlepack$', views.d3circlepack, name='d3circlepack'),
+    url(r'^d3lineChart$', views.d3lineChart, name='d3lineChart'),    
     url(r'^d3multiLineChart$', views.d3multiLineChart, name='d3multiLineChart'),
+    url(r'^d3sunburst$', views.d3sunburst, name='d3sunburst'),  
+
+      
 ]
--- d3/views.py
+++ d3/views.py
@@ -9,6 +9,18 @@ def d3circlepack(request):
 
   return render(request,'d3/d3circlepack.html') 
 
+def d3lineChart(request):
+
+  return render(request,'d3/d3lineChart.html') 
+
 def d3multiLineChart(request):
 
-  return render(request,'d3/d3multiLineChart.html') 
+  return render(request,'d3/d3multiLineChart.html')  
+
+def d3barChart(request):
+
+  return render(request,'d3/d3barChart.html') 
+
+def d3sunburst(request):
+
+  return render(request,'d3/d3sunburst.html')     
