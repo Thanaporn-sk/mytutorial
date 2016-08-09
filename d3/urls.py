ad543d81eac59e9f4eec30651c5125dc20b5460b
Thanaporn-sk <thanaporn.sk@gmail.com>
2016-08-09 17:03:46 +0700

    apply template
    apply template : menu

--- d3/urls.py
+++ d3/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^d3$', views.d3Chain, name='d3Chain'),
     url(r'^d3circlepack$', views.d3circlepack, name='d3circlepack'),
     url(r'^d3lineChart$', views.d3lineChart, name='d3lineChart'),    
+        url(r'^d3barChart$', views.d3barChart, name='d3barChart'),   
     url(r'^d3multiLineChart$', views.d3multiLineChart, name='d3multiLineChart'),
     url(r'^d3sunburst$', views.d3sunburst, name='d3sunburst'),  
 
